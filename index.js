744f7550ead1195bfe2c43b799291912cdb6e065
Jared Forsyth <jared@jaredforsyth.com>
2013-10-28 13:28:09 -0600

    wip

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var Runner = require('strider-simple-runner').Runner
 
   , runDocker = require('./lib')
 
-function create(emitter, opts, cb) {
-  opts.processJob = runDocker
-  var runner = new Runner(emitter, opts)
-  cb(null)
+function create(emitter, config, context, done) {
+  config = config || {}
+  config.processJob = runDocker
+  var runner = new Runner(emitter, config)
+  runner.loadExtensions(context.extensionPaths, function (err) {
+    done(err, runner)
+  })
 }
 
 module.exports = {
