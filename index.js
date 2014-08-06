9a44c128d78b073f7299b75877b2689e1d9402d9
Jared Forsyth <jared@khanacademy.org>
2014-08-06 02:40:58 -0700

    intial pass -- we have a working build!

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
-var Runner = require('./lib')
+var Runner = require('strider-simple-runner').Runner
+  , initDocker = require('./lib/init')
+  , runDocker = require('./lib/run')
 
-function create(emitter, config, context, done) {
-  var runner = new Runner(emitter, config)
-  runner.init(function (err) {
+var create = function(emitter, config, context, done){
+  config = config || {}
+  initDocker(config.host, config.port, function (err, docker) {
     if (err) {
       console.warn("Failed to initialize docker! Make sure permissions are right, config is right, etc.", err)
       return done(new Error('Cannot initialize docker'))
     }
-    done(null, runner)
+
+    config.processJob = runDocker.bind(null, docker)
+    var runner = new Runner(emitter, config)
+    runner.id = 'docker'
+    runner.loadExtensions(context.extensionPaths, function (err) {
+      done(err, runner)
+    })
   })
 }
 
@@ -18,9 +26,9 @@ module.exports = {
     /** TODO use this
     method: String, // socket | http
     socketPath: String, // /var/run/docker.sock
+    */
     host: String, // 127.0.0.1
     post: Number, // 3000
-    */
   }
 }
 
