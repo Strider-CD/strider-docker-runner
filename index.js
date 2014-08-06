a5bdeb1871caed37d6124277520d0ade01617d91
Jared Forsyth <jared@khanacademy.org>
2014-08-05 22:48:44 -0700

    second pass wip

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
-var Runner = require('strider-simple-runner').Runner
-
-  , runDocker = require('./lib')
+var Runner = require('./lib')
 
 function create(emitter, config, context, done) {
-  config = config || {}
-  config.processJob = runDocker
   var runner = new Runner(emitter, config)
-  runner.loadExtensions(context.extensionPaths, function (err) {
-    done(err, runner)
+  runner.init(function (err) {
+    if (err) {
+      console.warn("Failed to initialize docker! Make sure permissions are right, config is right, etc.", err)
+      return done(new Error('Cannot initialize docker'))
+    }
+    done(null, runner)
   })
 }
 
 module.exports = {
-  create: create
+  create: create,
+  config: {
+    /** TODO use this
+    method: String, // socket | http
+    socketPath: String, // /var/run/docker.sock
+    host: String, // 127.0.0.1
+    post: Number, // 3000
+    */
+  }
 }
 
